3b1589f80c26e2c3ad7ded2b59ec3d5b2c0868f1
Jon Q <hello@jonquach.com>
2018-12-08 21:23:56 -0500

    Add interesting storybook examples

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,7 +3,9 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import '../src/index'
 
-storiesOf('Resize', module).add('Example', () => {
+const stories = storiesOf('Resize', module)
+
+stories.add('Example', () => {
   class Example extends React.PureComponent {
     state = {
       event: true,
@@ -91,7 +93,8 @@ storiesOf('Resize', module).add('Example', () => {
           </div>
           <textarea
             ref={this.setNodeRef}
-            placeholder="Resize me. Square will change color on resizeEnd"
+            placeholder="Resize me. Square opacity + size will change."
+            style={{width: 400, height: 300}}
           />
           <br />
           <div
@@ -100,11 +103,291 @@ storiesOf('Resize', module).add('Example', () => {
               background: 'blue',
               color: 'white',
               transition: 'all 0.2s ease',
+              width: 200,
+              height: 150,
             }}
           >
             I'll be half the size + an animation delay
           </div>
           <br />
+        </div>
+      )
+    }
+  }
+
+  return <Example />
+})
+
+stories.add('Chaining: Resize', () => {
+  class Example extends React.PureComponent {
+    state = {
+      event: true,
+      eventData: {},
+    }
+    componentDidMount() {
+      this.node.addEventListener('resize', this.onResizeEnd)
+      this.nodeOne.addEventListener('resize', this.onResizeEndOne)
+      this.nodeTwo.addEventListener('resize', this.onResizeEndTwo)
+    }
+
+    componentWillUnmount() {
+      this.node.removeEventListener('resize', this.onResizeEnd)
+      this.nodeOne.removeEventListener('resize', this.onResizeEndOne)
+      this.nodeTwo.removeEventListener('resize', this.onResizeEndTwo)
+    }
+
+    onResizeEnd = ({width, height}) => {
+      this.nodeOne.style.width = `${width}px`
+      this.nodeOne.style.height = `${height}px`
+    }
+
+    onResizeEndOne = ({width, height}) => {
+      this.nodeTwo.style.width = `${width}px`
+      this.nodeTwo.style.height = `${height}px`
+    }
+
+    onResizeEndTwo = ({width, height}) => {
+      this.nodeThree.style.width = `${width}px`
+      this.nodeThree.style.height = `${height}px`
+    }
+
+    setNodeRef = node => (this.node = node)
+    setNodeOne = node => (this.nodeOne = node)
+    setNodeTwo = node => (this.nodeTwo = node)
+    setNodeThree = node => (this.nodeThree = node)
+
+    render() {
+      return (
+        <div>
+          <textarea
+            ref={this.setNodeRef}
+            placeholder="Resize me. Watch what happens when you stop."
+            style={{
+              position: 'fixed',
+              zIndex: 100,
+              top: 0,
+              left: 0,
+              opacity: 0.5,
+            }}
+          />
+          <div
+            ref={this.setNodeOne}
+            style={{
+              background: 'red',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Textarea
+          </div>
+          <div
+            ref={this.setNodeTwo}
+            style={{
+              background: 'green',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Red
+          </div>
+          <div
+            ref={this.setNodeThree}
+            style={{
+              background: 'blue',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Green
+          </div>
+          <br />
+        </div>
+      )
+    }
+  }
+
+  return <Example />
+})
+
+stories.add('Chaining: ResizeEnd', () => {
+  class Example extends React.PureComponent {
+    state = {
+      event: true,
+      eventData: {},
+    }
+    componentDidMount() {
+      this.node.addEventListener('resizeEnd', this.onResizeEnd)
+      this.nodeOne.addEventListener('resizeEnd', this.onResizeEndOne)
+      this.nodeTwo.addEventListener('resizeEnd', this.onResizeEndTwo)
+    }
+
+    componentWillUnmount() {
+      this.node.removeEventListener('resizeEnd', this.onResizeEnd)
+      this.nodeOne.removeEventListener('resizeEnd', this.onResizeEndOne)
+      this.nodeTwo.removeEventListener('resizeEnd', this.onResizeEndTwo)
+    }
+
+    onResizeEnd = ({width, height}) => {
+      this.nodeOne.style.width = `${width}px`
+      this.nodeOne.style.height = `${height}px`
+    }
+
+    onResizeEndOne = ({width, height}) => {
+      this.nodeTwo.style.width = `${width}px`
+      this.nodeTwo.style.height = `${height}px`
+    }
+
+    onResizeEndTwo = ({width, height}) => {
+      this.nodeThree.style.width = `${width}px`
+      this.nodeThree.style.height = `${height}px`
+    }
+
+    setNodeRef = node => (this.node = node)
+    setNodeOne = node => (this.nodeOne = node)
+    setNodeTwo = node => (this.nodeTwo = node)
+    setNodeThree = node => (this.nodeThree = node)
+
+    render() {
+      return (
+        <div>
+          <textarea
+            ref={this.setNodeRef}
+            placeholder="Resize me. Watch what happens when you stop."
+            style={{
+              position: 'fixed',
+              zIndex: 100,
+              top: 0,
+              left: 0,
+              opacity: 0.5,
+            }}
+          />
+          <div
+            ref={this.setNodeOne}
+            style={{
+              background: 'red',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Textarea
+          </div>
+          <div
+            ref={this.setNodeTwo}
+            style={{
+              background: 'green',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Red
+          </div>
+          <div
+            ref={this.setNodeThree}
+            style={{
+              background: 'blue',
+              display: 'block',
+              float: 'left',
+              margin: 10,
+              transition: 'all 0.2s ease',
+              width: 100,
+              height: 100,
+            }}
+          >
+            I react to Green
+          </div>
+          <br />
+        </div>
+      )
+    }
+  }
+
+  return <Example />
+})
+
+stories.add('Stress Test', () => {
+  class Example extends React.PureComponent {
+    state = {
+      items: [...Array(200)].map((i, index) => ({id: `item-${index}`})),
+      width: 100,
+      height: 100,
+      resizing: false,
+    }
+
+    componentDidMount() {
+      this.node.addEventListener('resize', this.onResize)
+      this.node.addEventListener('resizeEnd', this.onResizeEnd)
+    }
+    componentWillUnmount() {
+      this.node.removeEventListener('resize', this.onResize)
+      this.node.addEventListener('resizeEnd', this.onResizeEnd)
+    }
+
+    onResize = event => {
+      const {width, height} = event
+      this.setState({
+        height: height,
+        width: width,
+        resizing: true,
+      })
+    }
+
+    onResizeEnd = event => {
+      this.setState({
+        resizing: false,
+      })
+    }
+
+    setNodeRef = node => (this.node = node)
+
+    render() {
+      const {height, width} = this.state
+
+      return (
+        <div>
+          <textarea
+            style={{position: 'fixed', zIndex: 100, width: 100, height: 100}}
+            ref={this.setNodeRef}
+            placeholder="Resize me. Watch the squares move"
+          />
+          {this.state.items.map((item, index) => (
+            <div
+              key={item.id}
+              style={{
+                width: 40,
+                height: 40,
+                display: 'block',
+                float: 'left',
+                background: 'blue',
+                opacity: this.state.resizing ? 0.3 : 1,
+                margin: 5,
+                transition: `all ${
+                  10 * index > 5000 ? 5000 : 10 * index
+                }ms linear`,
+                transform: `scaleX(${height / 100}) scaleY(${width / 100})`,
+                willChange: 'backgroun transform',
+              }}
+            />
+          ))}
         </div>
       )
     }
